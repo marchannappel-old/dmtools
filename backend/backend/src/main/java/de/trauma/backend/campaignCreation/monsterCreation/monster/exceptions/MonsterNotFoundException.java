fa1176fcd173a66aa8fa226d1afa32e2b52f099e
Marc Hannappel <thedevguymarc@gmail.com>
2023-08-11 23:38:31 +0200

    Implemented monster logic

---
+++ backend/backend/src/main/java/de/trauma/backend/campaignCreation/monsterCreation/monster/exceptions/MonsterNotFoundException.java
@@ -0,0 +1,7 @@
+package de.trauma.backend.campaignCreation.monsterCreation.monster.exceptions;
+
+public class MonsterNotFoundException extends RuntimeException {
+    public MonsterNotFoundException(String message) {
+        super(message);
+    }
+}
